30213ea9fe0383ed619ba286ab4a5d1a0620c66f
Ladmin <Ladmin@NB-WEPPER.TRNO.local>
2018-05-23 19:28:40 +0200

    Merge branch 'master' of C:\Users\Wepper\AndroidStudioProjects\Datenbanktest with conflicts.

--- app/src/main/java/com/sportfest/grundschul/datenbanktest/MainActivity.java
+++ app/src/main/java/com/sportfest/grundschul/datenbanktest/MainActivity.java
@@ -5,8 +5,18 @@ import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 
 import java.io.BufferedReader;
 import java.io.IOException;
@@ -15,15 +25,70 @@ import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.HashMap;
+import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
     String json_string;
 
+    //Deklaration Variablen Sprung
+    EditText Satz1, Satz2, Satz3, Springer;
+    Button Springen;
+    RequestQueue requestQueue;
+    //URL Location
+    String insertURL = "http://91.67.242.37/json_insert.php";
+
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //Refernzierung der Felder
+
+        Satz1 = (EditText) findViewById(R.id.Satz1);
+        Satz2 = (EditText) findViewById(R.id.Satz2);
+        Satz3 = (EditText) findViewById(R.id.Satz3);
+        Springer= (EditText) findViewById(R.id.Springer);
+        Springen = (Button) findViewById(R.id.springen);
+
+        requestQueue = Volley.newRequestQueue(getApplicationContext());
+
+        Springen.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                StringRequest request = new StringRequest(Request.Method.POST, insertURL,
+                        new Response.Listener<String>() {
+                            @Override
+                            public void onResponse(String response) {
+
+                            }
+                        }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+
+                    }
+                }){
+
+                    @Override
+                    protected Map<String, String> getParams() throws AuthFailureError {
+                        Map<String,String> parameters = new HashMap<String, String>();
+                        parameters.put("Springer", Springer.getText().toString());
+                        parameters.put("Satz1", Satz1.getText().toString());
+                        parameters.put("Satz2", Satz2.getText().toString());
+                        parameters.put("Satz3", Satz3.getText().toString());
+
+                        return parameters;
+                    }
+                };
+                requestQueue.add(request);
+
+            }
+        });
+
     }
 
 
