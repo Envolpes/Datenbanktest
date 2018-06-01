28c72b2374e7326e4de6086a569558de67a78e42
Nehemia <nehemia.tuerk@gmail.com>
2018-06-01 13:10:21 +0200

    Login

---
+++ app/src/main/java/com/sportfest/grundschul/datenbanktest/Login.java
@@ -0,0 +1,48 @@
+package com.sportfest.grundschul.datenbanktest;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+import android.widget.Toast;
+
+public class Login extends AppCompatActivity {
+
+    private EditText txtUserName, txtPassword;
+    private TextView txtView1;
+    private Button btnLogIn;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        txtUserName = (EditText) findViewById(R.id.txtUsername);
+        txtUserName.setText("Nehemia");
+        txtPassword = (EditText)   findViewById(R.id.txtPassword);
+        txtPassword.setText("1234");
+        txtView1 = (TextView) findViewById(R.id.txtView1);
+        btnLogIn = findViewById(R.id.btnLogIn);
+
+        btnLogIn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                validateLogIn(txtUserName.getText().toString(), txtPassword.getText().toString());
+            }
+        });
+    }
+
+    private void validateLogIn (String txtUserName, String txtPassword){
+        if((txtUserName.equals("Nehemia")) && (txtPassword.equals("1234"))){
+            Intent goToHome = new Intent(getApplicationContext(), MainActivity.class);
+            startActivity(goToHome);
+            Toast.makeText(getApplicationContext(),"Erfolgreich Eingeloggt",Toast.LENGTH_SHORT).show();
+        }
+        else{
+            txtView1.setText("Username oder Password falsch");
+        }
+    }
+}
